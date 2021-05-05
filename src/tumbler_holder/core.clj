d14bbcf485b53a18ddd64e0beb670277b7196468
kareinonitsuke <>
2021-05-05 20:43:51 +0900

    [clean] 変数を使用するように変更中

--- src/tumbler_holder/core.clj
+++ src/tumbler_holder/core.clj
@@ -2,45 +2,60 @@
   (:require [scad-clj.scad :as scad]
             [scad-clj.model :as model]))
 
+
+(def cup-hold-bottom-height 5)
+(def cup-hold-out 45)
+(def cup-hold-in 40)
+(def cup-hold-height 10)
+
+(def pipe-hold-out 17.5)
+(def pipe-hold-in 11.75)
+(def pipe-hold-wall-height 30)
+
+(def conect-cube-x 12)
+(def conect-cube-y 20)
+(def conect-cube-z cup-hold-height)
+
+
 (def primitives
   (model/union
-    (model/translate [45 0 0]
+    (model/translate [cup-hold-out 0 0]
       (model/difference
-        (model/cylinder 45 10 :center false)
-        (model/cylinder 40 15 :center false)))
+        (model/cylinder cup-hold-out cup-hold-height :center false)
+        (model/cylinder cup-hold-in cup-hold-height :center false)))
     (model/translate [0 0 0]
       (model/difference
         (model/translate [-20 0 0]
           (model/difference
-            (model/cylinder 17.5 30 :center false)
-            (model/cylinder 11.75 35 :center false)))
+            (model/cylinder pipe-hold-out pipe-hold-wall-height :center false)
+            (model/cylinder pipe-hold-in pipe-hold-wall-height :center false)))
         (model/union
-          (model/translate [-40 -15 0](model/cube 10 30 35 :center false))
-          (model/translate [-45 -10 0](model/cube 20 20 35 :center false)))
+          (model/translate [-40 -15 0](model/cube 10 30 pipe-hold-wall-height :center false))
+          (model/translate [-45 -10 0](model/cube 20 20 pipe-hold-wall-height :center false)))
       )
     )
-    (model/translate [-7 -10 0](model/cube 12 20 10 :center false))
+    (model/translate [-7 (- (/ conect-cube-y 2)) 0](model/cube conect-cube-x conect-cube-y conect-cube-z :center false))
   )
 )
 (def bottom-primitives
   (model/union
     (model/translate [45 0 0]
       (model/difference
-        (model/cylinder 45 30 :center false)
-        (model/translate [0 0 5](model/cylinder 40 30 :center false))
-        (model/translate [30 -10 5](model/cube 30 20 30 :center false))))
+        (model/cylinder 45 pipe-hold-wall-height :center false)
+        (model/translate [0 0 5](model/cylinder 40 pipe-hold-wall-height :center false))
+        (model/translate [30 -10 5](model/cube 30 20 pipe-hold-wall-height :center false))))
     (model/translate [0 0 0]
       (model/difference
         (model/translate [-20 0 0]
           (model/difference
-            (model/cylinder 17.5 30 :center false)
-            (model/cylinder 11.75 30 :center false)))
+            (model/cylinder 17.5 pipe-hold-wall-height :center false)
+            (model/cylinder 11.75 pipe-hold-wall-height :center false)))
         (model/union
-          (model/translate [-40 -15 0](model/cube 10 30 30 :center false))
-          (model/translate [-45 -10 0](model/cube 20 20 30 :center false)))
+          (model/translate [-40 -15 0](model/cube 10 30 pipe-hold-wall-height :center false))
+          (model/translate [-45 -10 0](model/cube 20 20 pipe-hold-wall-height :center false)))
       )
     )
-    (model/translate [-7 -10 0](model/cube 12 20 30 :center false))
+    (model/translate [-7 -10 0](model/cube 12 20 pipe-hold-wall-height :center false))
   )
 )
 
@@ -49,5 +64,4 @@
   [& args]
   (spit "tumbler-holder.scad" (scad/write-scad primitives))
   (spit "bottom-tumbler-holder.scad" (scad/write-scad bottom-primitives))
-
 )
